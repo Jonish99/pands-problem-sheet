758799e27a135de0cb998a70e186dbf0fa000529
Jonish99 <77638521+Jonish99@users.noreply.github.com>
2021-02-18 09:59:22 +0000

    Update collatz.py
    #Comment update

--- collatz.py
+++ collatz.py
@@ -1,5 +1,8 @@
-"""
-"""
+"""A program that takes a positive integer as input.
+The programme performs a calculation on the current value, (starting with input).
+If the value is even it is divided by two and assigned to the current value variable
+If it is odd it is multiplied by 3 and 1 is added and then the value is assigned to current value.
+The program ends when the current value is 1"""
 #Author: Jon Ishaque
 
 #out put message and ask user for +ve int and assign input to variable
