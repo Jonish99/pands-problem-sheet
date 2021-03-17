1825924fac7ef6257849f63e808abb266a964cff
Jon Ish <jon_ish@yahoo.com>
2021-03-17 17:31:30 +0000

    sorted out the plot colors problem by swapping blue and and red

--- plottask.py
+++ plottask.py
@@ -11,45 +11,46 @@ A program that plots the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 
 import matplotlib.pyplot as plt
 import numpy as np 
 
-#create f(xa)=x arrays -'f'
-#x inputs constant for 3 functions
-xpoints =np.array(range(0,4))
-ypoints1 = xpoints 
+#create f(x)=x arrays
+#x inputs for funciton range 0 - 4
+#Range funcitons includes starts at first parameter and upto but not including 2nd parameter -(numpy.arange — NumPy v1.20 Manual, 2021)
+xPoints =np.array(range(0,5))
+yPointsf = xPoints 
 
 #create g(x)=x2 - 'g'
-ypoints2 = xpoints * 2
+yPointsg = xPoints * xPoints
 
 #create h(x)=x3 - 'h'
+yPointsh = xPoints * xPoints * xPoints
 
-ypoints3 = xpoints * 3
-
-
-#create s, add label so legend and line colors are synched
-
+#create plotss with labels for legend and defined colors, labels and defined width.
 #plot function f
-plt.plot(xpoints,ypoints1, label="f(x)=x")
+plt.plot(xPoints,yPointsf, label="f(x)=x", color='blue', linewidth = 2)
 
 #plot function g
-plt.plot(xpoints,ypoints2,label="g(x)=x2")
+plt.plot(xPoints,yPointsg, label="g(x)=x2", color='green', linewidth = 2)
+
 
 #plot function h
-plt.plot(xpoints,ypoints3,label="h(x)=x3")
+plt.plot(xPoints,yPointsh, label="h(x)=x3", color='red', linewidth = 2 )
+
 #add legend with best location
 plt.legend(loc="best")
-#
+
 #add a title
-plt.title(label="Plot showing 3 functions")
+plt.title(label="Plot Showing 3 Functions")
 #add label to y axis
 plt.ylabel=("Y axis")
 #add label to x axis
 plt.xlabel=("X axis")
 #show the plot
-print (ypoints3)
+
 plt.plot(range(5))
 
-#axes upto 8
-plt.xlim(0, 8)
-plt.ylim(0, 8)
+#xaxes upto 4
+plt.xlim(0, 4)
+#y axes max should be upto 64
+plt.ylim(0, 64)
 
 
 #show the plot
